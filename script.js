d1df8bfa5ce73d610655520bfd3d5caf1b063f99
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-06-23 00:33:15 +0600

    fully completed the project

--- script.js
+++ script.js
@@ -12,5 +12,35 @@ setInterval(() =>{
     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
     sec.style.transform = `rotateZ(${ss}deg)`;
+
+
+
+// ==========digital clock
+let hours = document.querySelector('#hour');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+let ampm = document.querySelector('#ampm');
+
+let h = day.getHours();
+let m = day.getMinutes();
+let s = day.getSeconds();
+let am = h >= 12 ? "PM" : "AM";
+
+if(h > 12){
+    h = h - 12;
+}
+if(h == 0){
+    h = 12;
+}
+
+h = (h < 10) ? '0' + h : h;
+m = (m < 10) ? '0' + m : m;
+s = (s < 10) ? '0' + s : s;
+
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
+ampm.innerHTML = am;
+
 });
 
